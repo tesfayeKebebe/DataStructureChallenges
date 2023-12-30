fb7b3015f3c2f74b48ee2e2fb92ca9e3d75b0d9d
tesfayeKebebe <42234406+tesfayeKebebe@users.noreply.github.com>
2023-12-30 17:54:11 +0300

    adding mathimatical challanges

--- PracticeAlgorithm/MathematicsChallenge.cs
+++ PracticeAlgorithm/MathematicsChallenge.cs
@@ -12,12 +12,33 @@ public class MathematicsChallenge
 
     }
 
-    [Fact]
-    public void CheckFibonacci()
+    [Theory]
+    [InlineData(13,new int[] {0,1, 1, 2, 3, 5, 8, 13})]
+    [InlineData(8,new int[] {0,1, 1, 2, 3, 5, 8})]
+    public void CheckFibonacci(int number, int[] expected)
     {
-        var result = Fibonacci(13);
-        result.Should().BeEquivalentTo(new int[] {0,1, 1, 2, 3, 5, 8, 13});
+        var result = Fibonacci(number);
+        result.Should().BeEquivalentTo(expected);
 
+    }
+
+    [Theory]
+    [InlineData(2, true)]
+    [InlineData(5, true)]
+    [InlineData(6, false)]
+    public void CheckPrimeNumber(int number, bool expected)
+    {
+        var result = IsPrime(number);
+        result.Should().Be(expected);
+    }
+    [Theory]
+    [InlineData(24, 36, 12)]
+    [InlineData(40, 8, 8)]
+    [InlineData(12, 36, 12)]
+    public void CheckGreatestCommonDivisor(int n1, int n2, int expected)
+    {
+        var result = GreatestCommonDivisor(n1, n2);
+        result.Should().Be(expected);
     }
     private int Factorial(int n)
     {
@@ -28,7 +49,11 @@ public class MathematicsChallenge
         }
         return fact;
     }
-
+/// <summary>
+/// Definition: The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones. It starts with 0 and 1 (or 1 and 1).
+/// </summary>
+/// <param name="limit"></param>
+/// <returns></returns>
     private int[] Fibonacci(int limit)
     {
         var sequence = new List<int>();
@@ -42,5 +67,36 @@ public class MathematicsChallenge
             b = temp + b;
         }
         return sequence.ToArray()  ;
+    }
+/// <summary>
+/// Definition: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+/// </summary>
+/// <param name="n"></param>
+/// <returns></returns>
+    private bool IsPrime(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+        for (var i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int GreatestCommonDivisor(int n1, int n2)
+    {
+        while (n2!=0)
+        {
+            var temp = n2;
+            n2 = n1%n2;
+            n1 = temp;
+        }
+        return n1;
     }
 }
